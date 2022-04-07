6d42c294656eb53e3d1fc551c709294294ebcd6d
fbarfvestam <filip.barfvestam@chasacademy.se>
2022-04-07 18:25:38 +0200

    get a recipe for userlists and recipelists

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RecipeList;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    //
+
+    public function getRecipe($id)
+    {
+        $recipe = RecipeList::where('user__list_id', $id)->get();
+        $response = [
+            'status' => true,
+            'message' => $recipe
+        ];
+
+        return response($response, 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\ListController;
+use App\Http\Controllers\RecipeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,10 +18,10 @@ use App\Http\Controllers\ListController;
 |
 */
 
-Route::middleware('auth:sanctum')->group( function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::resource('users', UserController::class);
     Route::get('/users/{id}', [AuthController::class, 'getUser']);
-   /*  Route::post('create-list', ) */
+    /*  Route::post('create-list', ) */
 });
 
 Route::post('login', [AuthController::class, 'login']);
@@ -29,6 +30,4 @@ Route::post('logout', [AuthController::class, 'logout']);
 Route::post('create-list/{id}', [ListController::class, 'store']);
 Route::delete('delete-list/{id}', [ListController::class, 'destroy']);
 Route::get('get-list/{id}', [ListController::class, 'showList']);
-
-
-
+Route::get('get-recipe/{id}', [RecipeController::class, 'getRecipe']);
